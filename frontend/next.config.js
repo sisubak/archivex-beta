c1da140983317909c264f55e10037384372ab280
real_ueh <157288345+sisubak@users.noreply.github.com>
2026-06-01 02:59:26 +0300

    frontend: enable static export for github pages

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+output: 'export',
+images: { unoptimized: true },
+trailingSlash: true,
+reactStrictMode: true,
+};
+
 module.exports = nextConfig;
