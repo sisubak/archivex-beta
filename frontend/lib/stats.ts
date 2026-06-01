3c4b70fb6b6dfb1f4f96b804a3168a6917e666d6
real_ueh <157288345+sisubak@users.noreply.github.com>
2026-06-01 14:28:10 +0300

    Update stats.ts

--- frontend/lib/stats.ts
+++ frontend/lib/stats.ts
@@ -1,6 +1,6 @@
 export const SITE_STATS = {
-  clients: 50,
+  clients: 53,
   arts: 14,
-  community: 3556,
+  community: 3568,
   chatImageUrl: "https://i.postimg.cc/cHXPFHwP/izobrazenie.png",
 };
